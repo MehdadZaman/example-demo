8fb7a5e4243ffd76d2a1c179b028760f81815ec5
jahin imtiaz <jahinimtiaz@gmail.com>
2020-01-29 19:05:17 -0500

    added Daily Log Page

---
+++ app/src/main/java/android/example/nutrilline/DailyLogPage.java
@@ -0,0 +1,29 @@
+package android.example.nutrilline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DailyLogPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_log_page);
+    }
+
+}
