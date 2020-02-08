d2275f5fb99f247ac5db62e6284068ef23cfa000
Mehdad Zaman <zaman.mehdad.227@gmail.com>
2020-02-08 15:55:17 -0500

    addition of set max nutrtion page

---
+++ app/src/main/java/android/example/nutrilline/DoctorSetMaxIntakesPage.java
@@ -0,0 +1,14 @@
+package android.example.nutrilline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorSetMaxIntakesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_set_max_intakes_page);
+    }
+}
