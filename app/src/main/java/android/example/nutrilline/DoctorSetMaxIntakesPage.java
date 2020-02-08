1ae76d687771ba55cdc907769798c82cffd8914e
Mehdad Zaman <zaman.mehdad.227@gmail.com>
2020-02-08 16:54:36 -0500

    setmax intakes page complete

--- app/src/main/java/android/example/nutrilline/DoctorSetMaxIntakesPage.java
+++ app/src/main/java/android/example/nutrilline/DoctorSetMaxIntakesPage.java
@@ -2,7 +2,10 @@ package android.example.nutrilline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DoctorSetMaxIntakesPage extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class DoctorSetMaxIntakesPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_set_max_intakes_page);
+    }
+
+    public void setMaxIntakesDoctor(View v)
+    {
+        Toast.makeText(this, "User's maximum intakes updated!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, PatientsListPage.class);
+        startActivity(intent);
     }
 }
